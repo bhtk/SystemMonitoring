0f340ca402e8ae1d2612fa9643b4eff9630acb91
Behzad <bhtk.2005@gmail.com>
2015-08-29 13:45:54 +0430

    Add ServiceOutput to the projects

---
+++ SystemMonitor/Models/UtilityModels/ServiceOutput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemMonitor.Models.UtilityModels
+{
+    public class ServiceOutput
+    {
+        public long JobId { get; set; }
+        public long ClientId { get; set; }
+        public long Result { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
