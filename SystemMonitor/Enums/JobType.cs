c89167b9cfd86d7e738cd3e7da778156d08d319a
Behzad <bhtk.2005@gmail.com>
2015-08-30 21:34:31 +0430

    Add enums namespace to the solution and add JobType enum to the project

---
+++ SystemMonitor/Enums/JobType.cs
@@ -0,0 +1,11 @@
+﻿namespace SystemMonitor.Enums
+{
+    public enum JobType
+    {
+        PingRequest = 1,
+        HttpRequest = 2,
+        CpuUsage = 3,
+        MemorySpace = 4,
+        DiskSpace = 5
+    }
+}
